8e6031f608e64074b1839c305b123bc395d99d40
Marina Cerame <marina.cerame@gmail.com>
2016-12-10 18:54:11 -0600

    increased functionality for additional tests

---
+++ server-pure-sql/schema.sql
@@ -0,0 +1,32 @@
+
+DROP DATABASE IF EXISTS chat;
+
+
+CREATE DATABASE chat;
+
+USE chat;
+
+
+CREATE TABLE users (
+  id integer NOT NULL AUTO_INCREMENT,
+  username varchar(50),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  id integer NOT NULL AUTO_INCREMENT,
+  body varchar(145),
+  room varchar(50),
+  user varchar(50),
+  PRIMARY KEY (id)
+);
+
+/* Create other tables and define schemas for them here! */
+
+
+
+
+/*  Execute this file from the command line by typing:
+
+ *    mysql -u root -p <server/schema.sql
+ *  to create the database and the tables.*/
---
+++ server-pure-sql/spec/server-spec.js
@@ -0,0 +1,149 @@
+/* You'll need to have MySQL running and your Node server running
+ * for these tests to pass. */
+
+var mysql = require('mysql');
+var request = require('request'); // You might need to npm install the request module!
+var expect = require('chai').expect;
+
+describe('Persistent Node Chat Server', function() {
+  var dbConnection;
+
+  beforeEach(function(done) {
+    dbConnection = mysql.createConnection({
+      user: 'root',
+      password: 'rina',
+      database: 'chat'
+    });
+    dbConnection.connect();
+
+
+    var tablename = 'messages'; // TODO: fill this out
+
+
+    /* Empty the db table before each test so that multiple tests
+     * (or repeated runs of the tests) won't screw each other up: */
+    dbConnection.query('truncate ' + tablename);
+    dbConnection.query('truncate users', done);
+
+    // dbConnection.query('truncate ' + 'users', done);
+
+  });
+
+  afterEach(function() {
+    dbConnection.end();
+  });
+
+  it('Should insert posted messages to the DB', function(done) {
+    // Post the user to the chat server.
+    request({
+      method: 'POST',
+      uri: 'http://127.0.0.1:3000/classes/users',
+      json: { username: 'Valjean' }
+    }, function () {
+
+      // Post a message to the node chat server:
+      request({
+        method: 'POST',
+        uri: 'http://127.0.0.1:3000/classes/messages',
+        json: {
+          username: 'Valjean',
+          message: 'In mercys name, three days is all I need.',
+          roomname: 'Hello'
+        }
+      }, function () {
+        // Now if we look in the database, we should find the
+        // posted message there.
+
+        // TODO: You might have to change this test to get all the data from
+        // your message table, since this is schema-dependent.
+        var queryString = 'SELECT * FROM messages';
+        var queryArgs = [];
+
+        dbConnection.query(queryString, queryArgs, function(err, results) {
+          console.log(results);
+          // Should have one result:
+          expect(results.length).to.equal(1);
+
+          // TODO: If you don't have a column named text, change this test.
+
+          expect(results[0].body).to.equal('In mercys name, three days is all I need.');
+
+          done();
+        });
+      });
+    });
+  });
+
+  it('Should output all messages from the DB', function(done) {
+    // Let's insert a message into the db
+    var queryArgs = ['Men like you can never change!', 'main', 'faks'];
+    var queryString = `INSERT INTO messages(body, room, user) VALUES('${queryArgs[0]}', '${queryArgs[1]}', '${queryArgs[2]}')`;
+    // var queryArgs = [];
+
+    // TODO - The exact query string and query args to use
+    // here depend on the schema you design, so I'll leave
+    // them up to you. */
+
+    dbConnection.query(queryString, queryArgs, function(err) {
+      if (err) { throw err; }
+
+      // Now query the Node chat server and see if it returns
+      // the message we just inserted:
+      request('http://127.0.0.1:3000/classes/messages', function(error, response, body) {
+        console.log(body + ' body');
+        console.log(response.body, ' response');
+        var messageLog = JSON.parse(body);
+        expect(messageLog[0].body).to.equal('Men like you can never change!');
+        expect(messageLog[0].room).to.equal('main');
+        done();
+      });
+    });
+  });
+
+  it('Should return all users from the database', done => {
+
+    request({
+      method: 'POST',
+      uri: 'http://localhost:3000/classes/users',
+      json: { username: 'Captain Awesome'}
+    });
+
+    request('http://localhost:3000/classes/users', (error, response, body) => {
+      if (error) {
+        console.log('ERROR IN TEST', error);
+      }
+      console.log(response.body, '<================= THIS IS BODY');
+      var user = JSON.parse(body);
+      expect(user[0].username).to.equal('Captain Awesome');
+      done();
+    });
+  });
+
+  it('Should return appropriate status codes', done => {
+    request({
+      method: 'POST',
+      uri: 'http://127.0.0.1:3000/classes/users',
+      json: { username: 'Valjean' }
+    }, function () {
+
+      // Post a message to the node chat server:
+      request({
+        method: 'POST',
+        uri: 'http://127.0.0.1:3000/classes/messages',
+        json: {
+          username: 'Valjean',
+          message: 'In mercys name, three days is all I need.',
+          roomname: 'Hello'
+        }
+      }, function () {
+        request('http://localhost:3000/classes/users', (error, response, body) => {
+          if (error) {
+            console.log('ERROR IN TEST', error);
+          }
+          expect(response.statusCode).to.equal(200);
+          done();
+        });
+      });
+    });
+  });
+});
